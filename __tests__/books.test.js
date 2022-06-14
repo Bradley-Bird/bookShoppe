a41e01cd731b0a2d9ac8d2625ed1b9509d93ac50
Bradley-Bird <Bradleybird2@icloud.com>
2022-06-14 15:12:42 -0700

    booksbyid test passing route grabbing correct data

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -11,6 +11,21 @@ describe('book routes', () => {
     const res = await request(app).get('/books');
     expect(res.body.length).toEqual(5);
   });
+  it('displays book by id, with author', async () => {
+    const res = await request(app).get('/books/1');
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      authors: expect.arrayContaining([
+        {
+          id: expect.any(Number),
+          first_name: expect.any(String),
+          last_name: expect.any(String),
+          birth_date: expect.any(String),
+        },
+      ]),
+    });
+  });
   afterAll(() => {
     pool.end();
   });
